9fc79593f1820bca406fee4191c983ebb333b4a1
sam <ruwesam@gmail.com>
2021-07-08 08:20:27 -0400

    Wasm Working Initial

--- apps/wasm/src/app/app.component.ts
+++ apps/wasm/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'nx-workspace-root',
-  templateUrl: './app.component.html',
+  template: `{{value}}`,
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'wasm';
+export class AppComponent implements OnInit {
+
+  value: any = 'first value';
+
+  minusOne: any = () => 2;
+
+  ngOnInit(): void {
+    this.loadWasm();
+  }
+
+  async loadWasm() {
+    const response = await fetch('sam-wasm/optimized.wasm');
+    const {instance} = await WebAssembly.instantiateStreaming(response);
+    console.log(instance.exports);
+    this.minusOne = instance.exports?.minusOne;
+    this.value = this.minusOne(10);
+  }
+
 }
---
+++ apps/wasm/src/wasm/assembly/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Need type references for Assembly Script
+ */
+export function minusOne(n: i32): i32 {
+  return n - 1;
+}
