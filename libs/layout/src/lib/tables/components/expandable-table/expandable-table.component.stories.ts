a3b34378e11529c5b4e25ce608d5784aa5c224b4
sam <ruwesam@gmail.com>
2021-10-27 10:18:48 -0400

    Add Storybook Utils Directory

--- libs/layout/src/lib/tables/components/expandable-table/expandable-table.component.stories.ts
+++ libs/layout/src/lib/tables/components/expandable-table/expandable-table.component.stories.ts
@@ -12,35 +12,13 @@ import {
 } from '../../cell/cell';
 import { TableModule } from '../../table.module';
 import { ExpandableTableComponent } from './expandable-table.component';
+import { element, ELEMENT_DATA } from '../../../storybook-utils/mock-data/elements';
 
 function log(data: string) {
   console.log(data);
 }
 
-type element = { position: number, name: string, weight: number, symbol: string }
 const dateTimeExamples = ["2021-10-13T13:53:39.000+00:00", "2021-10-13T13:54:39.000+00:00", "2021-10-13T13:55:39.000+00:00"];
-const data = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-]
 
 const columns: Array<ColumnModel<element>> = [
   new Column({
@@ -69,7 +47,7 @@ const columns: Array<ColumnModel<element>> = [
 ];
 
 export default {
-  title: 'Expandable Table',
+  title: 'Tables/Components/Expandable Table',
   component: ExpandableTableComponent,
   decorators: [
     moduleMetadata({
@@ -81,6 +59,6 @@ export default {
 const expandedRowGenerator = (element: any) => stringCell({value: element.name});
 
 export const Primary = () => ({
-  props: {data, columns, expandedRowGenerator},
+  props: {data: ELEMENT_DATA, columns, expandedRowGenerator},
   template: `<pg-layout-expandable-table [elementData]="data" [columns]="columns" [expandedRowGenerator]="expandedRowGenerator"></pg-layout-expandable-table>`
 });
--- libs/layout/src/lib/tables/injectable-table-bases/injectable-component-table/injectable-component-table.component.stories.ts
+++ libs/layout/src/lib/tables/injectable-table-bases/injectable-component-table/injectable-component-table.component.stories.ts
@@ -7,6 +7,7 @@ import { CellBase } from '../../cell/cell.component';
 import { Cell } from '../../cell/cell';
 import { LinesModule } from '../../../lines/lines.module';
 import { ExpansionPanelModule } from '../../../expansion-panels/expansion-panel.module';
+import { element, ELEMENT_DATA } from '../../../storybook-utils/mock-data/elements';
 
 @Component({
   template: `
@@ -17,12 +18,12 @@ import { ExpansionPanelModule } from '../../../expansion-panels/expansion-panel.
       <pg-layout-two-fields-line [data1]="data.data1" [data2]="data.data2" content></pg-layout-two-fields-line>
       <pg-layout-two-fields-line [data1]="data.data1" [data2]="data.data2" content></pg-layout-two-fields-line>
       <pg-layout-two-fields-line [data1]="data.data1" [data2]="data.data2" content></pg-layout-two-fields-line>
-      <!--      <p content>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce accumsan porttitor enim et porta.-->
-      <!--        Donec magna turpis, fermentum quis diam sed, tempus molestie velit. Vivamus sollicitudin elementum lacus non-->
-      <!--        pellentesque. Phasellus posuere orci nibh, vel sodales massa efficitur quis. Maecenas fringilla orci ac nulla-->
-      <!--        sollicitudin lobortis. Donec rhoncus turpis sit amet dui iaculis scelerisque. Donec ac dictum lacus. Praesent-->
-      <!--        augue ex, volutpat id ultrices eget, vehicula a libero. Integer feugiat aliquet accumsan. Vestibulum vel commodo-->
-      <!--        eros, at ultricies tellus. Fusce quis rhoncus urna, in tempor arcu.</p>-->
+      <p content>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce accumsan porttitor enim et porta.
+        Donec magna turpis, fermentum quis diam sed, tempus molestie velit. Vivamus sollicitudin elementum lacus non
+        pellentesque. Phasellus posuere orci nibh, vel sodales massa efficitur quis. Maecenas fringilla orci ac nulla
+        sollicitudin lobortis. Donec rhoncus turpis sit amet dui iaculis scelerisque. Donec ac dictum lacus. Praesent
+        augue ex, volutpat id ultrices eget, vehicula a libero. Integer feugiat aliquet accumsan. Vestibulum vel commodo
+        eros, at ultricies tellus. Fusce quis rhoncus urna, in tempor arcu.</p>
     </pg-layout-expansion-panel>
   `
 })
@@ -32,37 +33,13 @@ function expandedCellComponent(data: any) {
   return new Cell(ExpandedCellComponent, data);
 }
 
-type element = { position: number, name: string, weight: number, symbol: string }
-const data = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-];
-
 const rowGenerator = (element: element) => expandedCellComponent({
   data1: {field: 'Name', value: element.name},
   data2: {field: 'Symbol', value: element.symbol}
 });
 
 export default {
-  title: 'Table with Component Injection',
+  title: 'Tables/Bases/Table with Component Injection',
   component: InjectableComponentTableComponent,
   decorators: [
     moduleMetadata({
@@ -73,6 +50,6 @@ export default {
 } as Meta<InjectableComponentTableComponent<any>>;
 
 export const Primary = () => ({
-  props: {data, rowGenerator},
+  props: {data: ELEMENT_DATA, rowGenerator},
   template: `<pg-layout-component-injectable-table [elementData]="data" [rowGenerator]="rowGenerator"></pg-layout-component-injectable-table>`
 });
